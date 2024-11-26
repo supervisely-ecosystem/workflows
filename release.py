fbe549700b142a1719b0d877116bdb5a408a8e98
Nikolai Petukhov <pnikolay01@icloud.com>
2024-11-26 13:46:04 -0300

    Update release.py

--- release.py
+++ release.py
@@ -690,6 +690,8 @@ def validate_docker_image(subapp_paths):
         except Exception:
             print(f"ERROR: Config file not found in subapp {subapp_name}")
             raise
+        if config["type"] == "project":
+            return
         docker_image = config["docker_image"].replace("supervisely/", "")
         skopeo_result = subprocess.run(["skopeo", "inspect", f"docker://docker.io/supervisely/{docker_image}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if skopeo_result.returncode != 0:
